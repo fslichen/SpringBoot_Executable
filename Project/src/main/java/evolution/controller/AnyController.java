15da4fe587eeb5977d0f450b1938c286109291fb
chenli <chenli@192.168.43.24>
2017-04-27 17:52:24 +0800

    refactor

--- Project/src/main/java/evolution/controller/AnyController.java
+++ Project/src/main/java/evolution/controller/AnyController.java
@@ -1,12 +1,21 @@
 package evolution.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnyController {
+	@Value("name")// You can inject the property directly from the properties file without @Bean.
+	private String name;
+	
 	@GetMapping("/get")
 	public String get() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/properties")
+	public String properties() {
+		return name;
 	}
 }
